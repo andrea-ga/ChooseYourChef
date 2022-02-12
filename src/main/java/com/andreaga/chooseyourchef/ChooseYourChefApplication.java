a73bbfabc964a9762448d3c2d2ded5e7f6582e70
jollypuppet <galella.andrea@gmail.com>
2022-02-12 10:46:58 +0100

    Initial commit

---
+++ src/main/java/com/andreaga/chooseyourchef/ChooseYourChefApplication.java
@@ -0,0 +1,13 @@
+package com.andreaga.chooseyourchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseYourChefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChooseYourChefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andreaga/chooseyourchef/ChooseYourChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreaga.chooseyourchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseYourChefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
